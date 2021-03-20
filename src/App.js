76ef16c60ed6de4b9757a286ff246d671063fed6
고서연 <seoyeon@goseoyeon-ui-MacBookPro.local>
2021-03-20 23:19:40 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 // Class Component
 class App extends Component {
   // State
   state = { isLoading: true, movies: [] };
 
+  // async/await 비동기 처리
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+  };
+
   // Called Immediately after component creation
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
-    // data fetching from API --> movie render
+    this.getMovies();
   }
 
   // render method
